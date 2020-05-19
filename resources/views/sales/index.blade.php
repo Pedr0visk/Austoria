b43db68d90960bc2135330b805a40bf10a5d33f8
pbelloto <pbelloto1997@gmail.com>
2020-05-19 10:35:53 -0300

    format on sales index removed

--- resources/views/sales/index.blade.php
+++ resources/views/sales/index.blade.php
@@ -30,7 +30,7 @@
                                     <td>{{ $sale->customer()->withTrashed()->first()->name }}</td>
                                     <td>{{ $sale->payment->pay_method_name }}</td>
                                     <td>{{ number_format($sale->subtotal, 2, ',', '.') }}</td>
-                                    <td>{{ number_format($sale->payment->amount, 2, ',', '.') }}</td>
+                                    <td>{{ $sale->payment->amount</td>
                                     <td>{{ $sale->created_at->format('d/m/Y') }}</td>
                                     <td><a href="{{ $sale->path() }}" class="btn btn-small btn-success"><i class="fa fa-eye"></i></a></td>
                                     <td>
